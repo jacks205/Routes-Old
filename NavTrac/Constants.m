d74ef82f93bfa530226fe3bb4b47b0e16ddb45b3
Mark Jackson <markjacks205@gmail.com>
2014-01-26 12:59:10 -0800

    Updated .gitignore

---
+++ NavTrac/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  NavTrac
+//
+//  Created by Mark Jackson on 1/20/14.
+//  Copyright (c) 2014 Mark Jackson. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
